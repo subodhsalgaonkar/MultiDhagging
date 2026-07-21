148a6b79ba5eefdca288d485f60a40e63291247d
subodhsalgaonkar <145000831+subodhsalgaonkar@users.noreply.github.com>
2026-07-22 03:02:10 +0530

    Got the thresholds somewhat right

---
+++ Section4/ImageProcessingPerformance.java
@@ -0,0 +1,120 @@
+package Section4;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageProcessingPerformance {
+
+    public static final String SOURCE_FILE = "./Assets/Flowers.jpg";
+    public static final String DEST_FILE = "./out/Flowers.jpg";
+    public static void main(String[] args) throws IOException {
+        BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+        BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),  BufferedImage.TYPE_INT_RGB);
+
+
+        recolorSingleThreaded(originalImage, resultImage);
+
+        File outputFile = new File(DEST_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+    }
+
+
+    /**
+     * Wrapper method for single threaded solution 
+     */
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage){
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+
+    /**
+     * Method that itterates through the original images and applies the recoloring
+     */
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner, int width, int height){
+        
+        for(int x = leftCorner; x < leftCorner + width; x++){
+            for(int y = topCorner; y < topCorner + height; y++){
+                recolorPixel(originalImage, resultImage, x, y);
+            }
+        }
+    }
+
+
+
+    /**
+     * Method to recolor each individual pixel
+     */
+    public static void recolorPixel(BufferedImage originImage, BufferedImage resultImage, int x, int y){
+        int rgb = originImage.getRGB(x, y);
+        
+        int red = getRed(rgb);
+        int blue = getBlue(rgb);
+        int green = getGreen(rgb);
+
+        int newRed;
+        int newBlue;
+        int newGreen;
+
+        if(isShadeOfGrey(red, green, blue)){
+            newRed = Math.max(0, red - 8);
+            newGreen =  Math.max(0, green - 50);
+            newBlue =  Math.max(0, blue - 10);
+        }
+        else{
+            newRed = red;
+            newGreen = green;
+            newBlue = blue;
+        }
+
+        int newRGB = createRGBFromColours(newRed, newGreen, newBlue);
+        setRGB(resultImage, x, y, newRGB);
+    }
+
+    /**
+     * Takes target buffered image object, pixel coordinated and rgb value and assigns the rgb value to the pixel in the result image 
+     */
+    public static void setRGB(BufferedImage image, int x, int y, int rgb){
+        image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+    }
+
+
+    /**
+     * Takes particular pixels value and determines if it's a shade of grey
+     * logic = all components almost same then shade of grey
+     */
+    public static boolean isShadeOfGrey(int red, int green, int blue){
+        return (Math.abs(red - green) < 50) && (Math.abs(red - blue) < 50) && (Math.abs(blue - green) < 50);
+        // I added 170 because even black pixels were treated as grey
+    }
+
+    /**
+     * Takes individual values of red blue and green and builds a compound pixel RGB value out of it.
+    */
+    public static int createRGBFromColours(int red, int green, int blue){
+        int rgb = 0;
+
+        rgb |= blue;
+        rgb |= (green << 8); 
+        rgb |= (red << 16);
+
+        rgb |= 0xFF000000; // setting alpha to max
+
+        return rgb;
+    }
+
+    public static int getBlue(int rgb){
+        return rgb & 0x000000FF;
+    }
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+}
--- Section4/README.md
+++ Section4/README.md
@@ -46,4 +46,53 @@ Performance improved by N
 
 N = subtasks /threads to break the original task
 
-But does breaskin tasks have a cost? and can we break any task?
+But does breaking tasks have a cost? and can we break any task?
+
+On a general purpose PC N should be close to number of cores
+        N = num. cores
+because *reduction in latency is a result of running these subtsaks fully in parallel*
+***and only if we run these tasks on differnet cores we can truly run them parallely***
+
+If noting else is running on the CPU, no I/O blocking, no sleep then OS will utilize hardware best but this happens rarely
+
+Adding 1 additional thread will also reduce latency as context switching, cache performance, extra memory consumption cause oveheads
+
+#### **Hyperthreading?**
+Also called virtual cores
+
+Most computer use this today
+Single physical core can run 2 threads at the time
+Achieved by having some hardware units duplicated in a physical core so two threads can run in parallel when some hardware units are shared 
+
+Cost of breaking tasks?
+we have to accept inherent cost of
+- some calculations to break tasks
+- time to create threads
+- time for thread to be scheduled by OS
+- time for longest task
+- time for aggregating thread takes
+
+When to parallelize/thread?
+Point on the intersection of latency vs task size for multithreaded and single threaded
+
+Can we break any task?
+No
+Type of tasks:
+- parallizable and easily breakable
+- unbreakable task which we are forced to run from start to finish
+- partially sequential and partially can be broken
+
+#### Summary
+
+- Performance is a broad term
+- multithreaded performance in Latency and Throughput
+- latency reduced by breakign tasks in smaller tasks
+
+
+# L2 Pratical of Image precessing using multithreading
+Topics: Image processing sequential, Image processing multithreaded, performance / latency measurements
+
+Task convert flowers to purpulish colors which looks natural
+
+pixelstores - ARGB
+A - alpha is transperency
